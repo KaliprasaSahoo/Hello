9f45b4a94919eb4c40fe749ff272da5e7a8f1adc
KaliprasaSahoo <69588229+KaliprasaSahoo@users.noreply.github.com>
2020-08-12 19:08:30 +0530

    FirstMethod added

--- hello.java
+++ hello.java
@@ -2,4 +2,7 @@ public class Hello{
 public static void main(string[] args){
 System.out.println("Hello java!!")
 }
+  public void FirstMethod(){
+  System.out.println("Hello First Method!!")
+  }
 }
